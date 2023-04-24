a19c3c2cc1a86a6fb9b9f7d3c6ef81561c1ea478
Elallagi99 <got.famme@gmail.com>
2023-04-24 12:15:30 -0700

    add strle file

---
+++ _strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - count the length of a character
+ * @s: the character to be counted
+ * Return: the length integer
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	return (len);
+}
