6d73a59978412df5a009623a5d2e3449edb370fc
Elallagi99 <got.famme@gmail.com>
2023-04-25 12:04:52 -0700

    print unsigned int

--- _print_u_.c
+++ _print_u_.c
@@ -5,9 +5,13 @@
  * _print_u_ - prints an unsigned integer
  * @number: the unsigned integer to print
  */
-void _print_u_(unsigned int number)
+int _print_u_(unsigned int number)
 {
+	int count = 0;
+
 	if (number > 9)
 		print_u_(number / 10);
 	_putchar((number % 10) + '0');
+	count++;
+	return (count);
 }
