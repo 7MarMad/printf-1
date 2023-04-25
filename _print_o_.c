b9cea3101323aadd04d97b18391a81763d1425db
Elallagi99 <got.famme@gmail.com>
2023-04-25 12:17:20 -0700

    print octal

--- _print_o_.c
+++ _print_o_.c
@@ -4,10 +4,15 @@
 /**
  * _print_o_ - prints the octal representation of a number
  * @number: the number to print
+ * Return: number of printed int
  */
-void _print_o_(unsigned int number)
+ 
+int _print_o_(unsigned int number)
 {
+	int count = 0;
+
 	if (number > 7)
 		_print_o_(number / 8);
 	_putchar((number % 8) + '0');
+	return (count++);
 }
